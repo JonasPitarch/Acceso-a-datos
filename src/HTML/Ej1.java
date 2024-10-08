e1d6723d6c0dad25af90ec1acc0ae7f7abea5dd4
jonpitsaf <jonaspitarch06@gmail.com>
2024-10-08 12:55:30 +0200

    ssg

--- src/HTML/Ej1.java
+++ src/HTML/Ej1.java
@@ -1,22 +1,33 @@
 package HTML;
 
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class Ej1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Dame el titulo de la web");
+        String titulo=scanner.nextLine();
+        System.out.println("Dame un elemento");
+        String texto=scanner.nextLine();
+        System.out.println("Dame el segundo elemento");
+        String texto2=scanner.nextLine();
+        System.out.println("Dame el tercer elemento");
+        String texto3=scanner.nextLine();
+        System.out.println("Dime un pequeño párrafo");
+        String texto4=scanner.nextLine();
         try (PrintWriter pw = new PrintWriter("archivo.html", "UTF-8")) {
-
             pw.println("<html>");
             pw.println("<head>");
-            pw.write("<title> Mi WEB </title>");
+            pw.write("<title>"+ titulo +"</title>");
             pw.println("</head>");
             pw.println("<body>");
             pw.println("<h1>Web de Jonás</h1>");
-            pw.println("<p>Esta es una línea de párrafo de html</p>");
+            pw.println("<p>"+texto4+"</p>");
             pw.println("<ul>");
-            pw.println("<li>Elemento1</li>");
-            pw.println("<li>Elemento2</li>");
-            pw.println("<li>Elemento3</li>");
+            pw.println("<li>"+texto+"</li>");
+            pw.println("<li>"+texto2+"</li>");
+            pw.println("<li>"+texto3+"</li>");
             pw.println("</ul>");
             pw.println("</body>");
             pw.println("</html>");
